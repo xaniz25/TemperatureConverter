f9303cddbec54b59977d9ac7d3a4f1347f2928d9
shanicetalan <shanicetalan@gmail.com>
2019-07-25 08:26:34 -0600

    Initial commit

---
+++ TemperatureConverter/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TemperatureConverter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Temperature Converter</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Temperature Converter</h3>
+            <br />
+            From:&nbsp;
+            <asp:DropDownList ID="FromDropDown" runat="server">
+                <asp:ListItem>Celsius</asp:ListItem>
+                <asp:ListItem>Farenheit</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; To:
+            <asp:DropDownList ID="ToDropDown" runat="server">
+                <asp:ListItem>Farenheit</asp:ListItem>
+                <asp:ListItem>Kelvin</asp:ListItem>
+                <asp:ListItem>Celsius</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            Input: <asp:TextBox ID="InputBox" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="InputBox" Display="Dynamic" ErrorMessage="Input is required" Font-Size="Medium" ForeColor="Red" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="InputBox" Display="Dynamic" ErrorMessage="Please enter a number from -1000 to 1000" Font-Size="Medium" ForeColor="Red" MaximumValue="1000" MinimumValue="-1000" Type="Double"></asp:RangeValidator>
+            <br />
+            <br />
+            <asp:Button ID="BtnCalculate" runat="server" Text="Calculate" OnClick="BtnCalculate_Click" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="BtnClear" runat="server" Text="Clear" OnClick="BtnClear_Click" />
+            <br />
+            <br />
+            Output: 
+            <asp:Label ID="OutputLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TemperatureConverter/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TemperatureConverter
+/*A web form that converts temperature from and to each type: Celsius, Farenheit, Kelvin
+ * using dropdown menus and outputs the result to a label control
+ * Created by Shanice Talan. July 17, 2019. CPRG214 OOSD Spring 2019
+ */
+{
+    public partial class Default : System.Web.UI.Page
+    {   //variable for storing input, the result before rounding, and the result after rounding
+        double input;
+        double result;
+        double rounded;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InputBox.Focus();
+        }
+
+        protected void BtnCalculate_Click(object sender, EventArgs e)
+        //calculate the result depending on the selection of the two dropdown boxes
+        {
+            Double.TryParse(InputBox.Text, out input); //converts the input to a double type
+
+            if (FromDropDown.SelectedValue == "Celsius" && ToDropDown.SelectedValue == "Farenheit")
+                result = input * 9 / 5 + 32;
+            else if (FromDropDown.SelectedValue == "Farenheit" && ToDropDown.SelectedValue == "Celsius")
+                result = (input - 32) * 5 / 9;
+            else if (FromDropDown.SelectedValue == "Celsius" && ToDropDown.SelectedValue == "Kelvin")
+                result = input + 273.15;
+            else if (FromDropDown.SelectedValue == "Kelvin" && ToDropDown.SelectedValue == "Celsius")
+                result = input - 273.15;
+            else if (FromDropDown.SelectedValue == "Farenheit" && ToDropDown.SelectedValue == "Kelvin")
+                result = (input - 32) * 5 / 9 + 273.15;
+            else if (FromDropDown.SelectedValue == "Kelvin" && ToDropDown.SelectedValue == "Farenheit")
+                result = (input - 273.15) * 9 / 5 + 32;
+            else //if converting to same type
+                result = input;
+
+            rounded = Math.Round(result,2); //round to 2 decimal points
+            OutputLabel.Text = rounded.ToString(); //write the rounded result to the output label
+        }
+
+        protected void BtnClear_Click(object sender, EventArgs e)
+        //resets the form to the starting position and clears input box and output label
+        {
+            FromDropDown.SelectedValue = "Celsius";
+            ToDropDown.SelectedValue = "Farenheit";
+            OutputLabel.Text = "";
+            InputBox.Text = "";
+            InputBox.Focus();
+        }
+    }
+}
